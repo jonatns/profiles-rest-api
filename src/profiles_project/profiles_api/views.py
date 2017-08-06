0db600c5b77505b1ee8bcdc039a3e5b16a8a8b47
Jonathan Navarrete <jonatns@gmail.com>
2017-08-05 22:40:26 -0700

    Created our first api view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+            'item 1',
+            'item2',
+            'item3'
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
