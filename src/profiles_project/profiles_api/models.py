deecbf12b25a3dd3a286c0e2fbc1ac2ab1a3dd99
Jonathan Navarrete <jonatns@gmail.com>
2017-08-06 00:47:38 -0700

    created migration and added Profile feed item

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -56,3 +56,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
+class ProfileFeedItem(models.Model):
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+
+        return self.status_text
